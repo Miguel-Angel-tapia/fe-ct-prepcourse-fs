98d405e20f21f8937bd588e5009e843a21d49cdd
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2023-09-21 17:17:28 -0300

    hola

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -1,73 +1,127 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function deObjetoAarray(objeto) {
-   // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
-   // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
-   // Estos elementos debe ser cada par clave:valor del objeto recibido.
-   // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
-   // Tu código:
+  // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
+  // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
+  // Estos elementos debe ser cada par clave:valor del objeto recibido.
+  // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
+  // Tu código:
+  let claveValor = Object.entries(objeto);
+  return claveValor;
 }
 
 function numberOfCharacters(string) {
-   // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
-   // letras del string, y su valor es la cantidad de veces que se repite en el string.
-   // Las letras deben estar en orden alfabético.
-   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
-   // Tu código:
+  // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
+  // letras del string, y su valor es la cantidad de veces que se repite en el string.
+  // Las letras deben estar en orden alfabético.
+  // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
+  // Tu código:
+  let resultado = {};
+  string = string.split("").sort().join("");
+  for (let i = 0; i < string.length; i++) {
+    resultado[string[i]] = resultado[string[i]] + 1 || 1;
+  }
+  return resultado;
 }
 
 function capToFront(string) {
-   // Recibes un string con algunas letras en mayúscula y otras en minúscula.
-   // Debes enviar todas las letras en mayúscula al comienzo del string.
-   // Retornar el string.
-   // [EJEMPLO]: soyHENRY ---> HENRYsoy
-   // Tu código:
+  // Recibes un string con algunas letras en mayúscula y otras en minúscula.
+  // Debes enviar todas las letras en mayúscula al comienzo del string.
+  // Retornar el string.
+  // [EJEMPLO]: soyHENRY ---> HENRYsoy
+  // Tu código:
+
+  let str = string.split("");
+
+  str.sort((m, M) => {
+    if (m === m.toUpperCase() && M === M.toLowerCase()) {
+      return -1;
+    } else if (m === m.toLowerCase() && M === M.toUpperCase()) {
+      return 1;
+    }
+  });
+  str = str.join("");
+  return str;
 }
 
 function asAmirror(frase) {
-   // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
-   // La diferencia es que cada palabra estará escrita al inverso.
-   // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
-   // Tu código:
+  // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
+  // La diferencia es que cada palabra estará escrita al inverso.
+  // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
+  // Tu código:
+  fraseEspejo = frase.split(" ");
+  for (let i = 0; i < fraseEspejo.length; i++) {
+    fraseEspejo[i] = fraseEspejo[i].split("").reverse().join("");
+  }
+  frase = fraseEspejo.join(" ");
+  return frase;
 }
 
 function capicua(numero) {
-   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
-   // Caso contrario: "No es capicua".
-   // Tu código:
+  // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
+  // Caso contrario: "No es capicua".
+  // Tu código:
+  num = numero.toString();
+  cap = num.split("").reverse().join("");
+
+  if (num === cap) {
+    return "Es capicua";
+  } else {
+    return "No es capicua";
+  }
 }
 
 function deleteAbc(string) {
-   // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
-   // Retorna el string sin estas letras.
-   // Tu código:
+  // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
+  // Retorna el string sin estas letras.
+  // Tu código:
+  string = string.split("");
+  let i = 0;
+  while (i < string.length) {
+    if (string[i] === "a" || string[i] === "b" || string[i] === "c") {
+      string.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  string = string.join("");
+  return string;
 }
 
 function sortArray(arrayOfStrings) {
-   // Recibes un arreglo de strings.
-   // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
-   // de la longitud de cada string.
-   // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
-   // Tu código:
+  // Recibes un arreglo de strings.
+  // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
+  // de la longitud de cada string.
+  // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
+  // Tu código:
+  arrayOfStrings.sort((a, b) => a.length - b.length);
+  return arrayOfStrings;
 }
 
 function buscoInterseccion(array1, array2) {
-   // Recibes dos arreglos de números.
-   // Debes retornar un nuevo arreglo en el que se guarden los elementos en común entre ambos arreglos.
-   // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
-   // Si no tienen elementos en común, retornar un arreglo vacío.
-   // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
-   // Tu código:
+  // Recibes dos arreglos de números.
+  // Debes retornar un nuevo arreglo en el que se guarden los elementos en común entre ambos arreglos.
+  // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
+  // Si no tienen elementos en común, retornar un arreglo vacío.
+  // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
+  // Tu código:
+  let nuevoArray = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      nuevoArray.push(array1[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   deObjetoAarray,
-   numberOfCharacters,
-   capToFront,
-   asAmirror,
-   capicua,
-   deleteAbc,
-   sortArray,
-   buscoInterseccion,
+  deObjetoAarray,
+  numberOfCharacters,
+  capToFront,
+  asAmirror,
+  capicua,
+  deleteAbc,
+  sortArray,
+  buscoInterseccion,
 };
